e2f07ff4f97457cf994ecb6006ad52ccbc50a2d8
Zuzull <113565616+ZulpaAgustin@users.noreply.github.com>
2024-05-15 13:41:27 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="navbar navbar-expand-lg" style="background-color: #ffffff">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="~/assets/img/LOGO-SMK4.webp" alt="" />
+      </a>
+      <div class="title me-auto">
+        <h2 class="m-0">PERPUS DIGITAL</h2>
+        <h4 class="m-0">SMKN 4 Tasikmalaya</h4>
+        <h6 class="m-0">Jl Depok Kel.Sukamenak Kec.Purbaratu Kota Tasikmalaya</h6>
+      </div>
+      <div class="collapse navbar-collapse" id="navbarNav"></div>
+      </div>
+  </nav>
+</template>
+
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;900&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@700&family=Poppins:wght@400;500;900&display=swap");
+
+h2 {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+}
+h4,
+h6 {
+  color: #5d5d5d;
+}
+
+nav {
+  color: #4377de;
+}
+</style>
