110aa104dfb0054883bcf1f0b57f766c159eecaf
Zuzull <113565616+ZulpaAgustin@users.noreply.github.com>
2024-05-27 11:36:12 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   css: ["bootstrap/dist/css/bootstrap.min.css"],
 
   modules: ["@nuxtjs/supabase"],
   supabase: { redirect: false },
+  app: {
+    head: {
+      script: [{
+        src: "https://cdn.jsdelivr.net/npm/bootstrap@5.2/dist/js/bootstrap.bundle.min.js"
+      }]
+    }
+  }
 });
