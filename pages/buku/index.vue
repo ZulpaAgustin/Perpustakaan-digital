3ba48b80255a7d25cd277abdc48b30b81041cd79
Zuzull <113565616+ZulpaAgustin@users.noreply.github.com>
2024-06-04 16:38:11 +0700

    Add files via upload

---
+++ pages/buku/index.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="container-fluid">
+    <div style="background-color: #4377de">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="icon mt-4">
+            <nuxt-link to="/pengunjung/menu">
+              <svg xmlns="http://www.w3.org/2000/svg" width="80" height="40" fill="currentColor" class="bi bi-arrow-left-square-fill text-white" viewBox="0 0 16 16">
+                <path d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1" />
+              </svg>
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+      <div class="row justify-content-between">
+        <div class="col-lg-8">
+          <h2 class="text-center ms-5">Pilih Buku</h2>
+        </div>
+        <div class="col-lg-4 d-flex justify-content-end align-items-center">
+          <div class="me-3">
+            <form @submit.prevent="getBooks">
+              <input v-model="keyword" type="search" class="form-control rounded-5 bi bi-search" placeholder="Cari buku disini..." />
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="ms-3 text-white fs-6">Menampilkan {{ books.length }} Dari {{ Semua }}</div>
+      <div class="row mt-5">
+        <div v-for="(book, i) in books" :key="i" class="col-3 d-flex flex-column justify-content-center align-items-center">
+          <div class="card mb-3" style="width: 16rem">
+            <div class="card-header" style="height: 350px">
+              <img :src="book.cover" alt="book.judul" class="card-img-top" />
+              <div class="card-body">
+                <h5 class="card-title text-center">{{ book.judul }}</h5>
+                <nuxt-link :to="`/buku/${book.id}`">
+                  <button type="submit" class="btn my-3">Buka</button>
+                </nuxt-link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: "Buku - Perpus Digital Zulpa"
+})
+const supabase = useSupabaseClient();
+
+const books = ref([]);
+const Semua = ref(0);
+const keyword = ref("");
+
+const getBooks = async () => {
+  const { data, error } = await supabase.from("Buku").select(`*, kategori(*)`).ilike("judul, ", `%${keyword.value}%`);
+  if (data) books.value = data;
+};
+
+const getSemuabuku = async () => {
+  const { data, count } = await supabase.from("Buku").select("*", { count: "exact" });
+  if (data) Semua.value = count;
+};
+
+onMounted(() => {
+  getBooks();
+  getSemuabuku();
+});
+
+</script>
+
+<style scoped>
+.cover {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  object-position: 0 30;
+}
+h2 {
+  color: #fff;
+  font-family: inherit;
+}
+.btn {
+  background-color: #315fb7;
+  color: #fff;
+}
+</style>
---
+++ pages/pengunjung/menu.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="container-fluid">
+    <div class="row justify-content-between">
+      <div class="col-1">
+        <div class="icon">
+          <nuxt-link to="/">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="80" fill="currentColor" class="bi bi-person-vcard-fill text-white" viewBox="0 0 16 16">
+              <path
+                d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm9 1.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 0-1h-4a.5.5 0 0 0-.5.5M9 8a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 0-1h-4A.5.5 0 0 0 9 8m1 2.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 0-1h-3a.5.5 0 0 0-.5.5m-1 2C9 10.567 7.21 9 5 9c-2.086 0-3.8 1.398-3.984 3.181A1 1 0 0 0 2 13h6.96q.04-.245.04-.5M7 6a2 2 0 1 0-4 0 2 2 0 0 0 4 0"
+              />
+            </svg>
+          </nuxt-link>
+        </div>
+      </div>
+      <div class="col-2 d-flex justify-content-end align-items-center">
+        <nuxt-link to="/buku/">
+          <button type="button" class="btn btn-bg">Cari Buku</button>
+        </nuxt-link>
+      </div>
+    </div>
+    <div class="row my-5">
+      <div class="col-lg-6">
+        <nuxt-link to="/pengunjung/" style="text-decoration: none">
+          <div class="card bg-pengunjung rounded-5">
+            <div class="card-body">
+              <h2>Riwayat Pengunjung</h2>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
+
+      <div class="col-lg-6">
+        <nuxt-link to="/buku/rekomen" style="text-decoration: none">
+          <div class="card bg-buku rounded-5">
+            <div class="card-body">
+              <h2>Rekomendasi Buku</h2>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
+    </div>
+
+    <!-- bagian kategori -->
+    <div class="oval">
+      <span class="badge text-bg-light">Kategori Buku Populer</span>
+    </div>
+
+    <div class="row row-cols-1 row-cols-md-5 g-5 mt-4 justify-content-center">
+  <div class="col">
+    <div class="card">
+      <img src="~/assets/img/comic-1.webp" class="card-img-top" alt="...">
+      <div class="card-body text-center">
+        <h6 class="card-title">Komik</h6>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="~/assets/img/mapel-1.webp" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h6 class="card-title text-center">Mata Pelajaran</h6>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="~/assets/img/novel-1.webp" class="card-img-top" alt="...">
+      <div class="card-body text-center">
+        <h6 class="card-title">Novel</h6>
+      </div>
+    </div>
+  </div>
+  
+</div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  height: 250px;
+  box-shadow: 1px 1px 10px #424242;
+}
+.card.bg-pengunjung {
+  background-image: url("img/kunjung.webp");
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  color: #ffffff;
+}
+.card.bg-buku {
+  background: url("img/cari.webp") no-repeat center center;
+  background-size: cover;
+  color: #ffffff;
+}
+.oval {
+  font-size: 30px;
+}
+.btn-bg {
+  background-color: #6194f6;
+  color: #ffffff;
+}
+.container-fluid {
+  background-color: #4377de;
+}
+</style>
+
+<script setup>
+useHead({
+  title: "Menu - Perpus Digital Zulpa"
+})
+</script>
