81f1f4ab792d0fe7d77b7b86770bb61ca1d1e57d
Zuzull <113565616+ZulpaAgustin@users.noreply.github.com>
2024-06-06 20:50:25 +0700

    Update index.vue

--- pages/buku/index.vue
+++ pages/buku/index.vue
@@ -5,8 +5,8 @@
         <div class="col-lg-12">
           <div class="icon mt-4">
             <nuxt-link to="/pengunjung/menu">
-              <svg xmlns="http://www.w3.org/2000/svg" width="80" height="40" fill="currentColor" class="bi bi-arrow-left-square-fill text-white" viewBox="0 0 16 16">
-                <path d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1" />
+              <svg aria-label="icon' xmlns="http://www.w3.org/2000/svg" width="80" height="40" fill="currentColor" class="bi bi-arrow-left-square-fill text-white" viewBox="0 0 16 16">
+                <path aria-label="back" d="M16 14a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2zm-4.5-6.5H5.707l2.147-2.146a.5.5 0 1 0-.708-.708l-3 3a.5.5 0 0 0 0 .708l3 3a.5.5 0 0 0 .708-.708L5.707 8.5H11.5a.5.5 0 0 0 0-1" />
               </svg>
             </nuxt-link>
           </div>
@@ -24,7 +24,7 @@
           </div>
         </div>
       </div>
-      <div class="ms-3 text-white fs-6">Menampilkan {{ books.length }} Dari {{ Semua }}</div>
+      <div class="ms-3 fs-6">Menampilkan {{ books.length }} Dari {{ Semua }}</div>
       <div class="row mt-5">
         <div v-for="(book, i) in books" :key="i" class="col-3 d-flex flex-column justify-content-center align-items-center">
           <div class="card mb-3" style="width: 16rem">
